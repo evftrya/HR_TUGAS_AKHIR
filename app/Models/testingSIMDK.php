cb81c4d85dbaa174d325a75c8748cd42c9ff62bf
evftrya <evfi29@gmail.com>
2026-04-29 01:50:30 +0700

    save

--- app/Models/testingSIMDK.php
+++ app/Models/testingSIMDK.php
@@ -8,17 +8,17 @@ class TestingSIMDK extends Model
 {
     // testing_s_i_m_d_k_s
     // use HasFactory;
-    
+
     protected $table = 'testing_s_i_m_d_k_s';
     protected $fillable = [
         'test_statuses',
         'users_id',
     ];
-    
+
     public $incrementing = true;
 
     protected $casts = [
-        'users_id' => 'string',  
+        'users_id' => 'string',
         'test_statuses' => 'array',
     ];
 }
