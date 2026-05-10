d49531b79ace10104aded4402db2d37cfbed795f
evftrya <evfi29@gmail.com>
2026-05-11 01:23:17 +0700

    sv

--- app/Http/Controllers/KelompokKeahlianController.php
+++ app/Http/Controllers/KelompokKeahlianController.php
@@ -71,7 +71,7 @@ class KelompokKeahlianController extends Controller
 
     public function create()
     {
-        $fakultas = Work_Position::where('tipe_work_position', 'Fakultas')->get();
+        $fakultas = Work_Position::where('type_work_position', 'Fakultas')->get();
         $this->MakeLog('User Mengakses halaman tambah Data '.$this->aksi);
 
         $route = view('kelola_data.kelompok_keahlian.input', compact('fakultas'));
